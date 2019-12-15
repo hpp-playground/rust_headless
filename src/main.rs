186e20756fe768789bf07f17184f8994ebe3cb95
hppRC <hpp.ricecake@gmail.com>
2019-12-16 00:35:15 +0900

    自分のサイトのスクショ職人にもなれる！

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use failure::Fallible;
+
+use headless_chrome::{protocol::page::ScreenshotFormat, Browser, LaunchOptionsBuilder};
+
+fn main() -> Fallible<()> {
+    let options = LaunchOptionsBuilder::default()
+        .headless(false)
+        .build()
+        .expect("Couldn't find appropriate Chrome binary.");
+    let browser = Browser::new(options)?;
+    let tab = browser.wait_for_initial_tab()?;
+
+    tab.navigate_to("https://hpprc.com")?
+        .wait_until_navigated()?
+        .wait_for_element("div.css-rm8i1b")?
+        .click()?;
+    tab.wait_until_navigated()?
+        .wait_for_element("div.css-1cngdp0")?
+        .click()?;
+    let hpp = tab.wait_until_navigated()?.capture_screenshot(
+        ScreenshotFormat::JPEG(Some(75)),
+        None,
+        true,
+    )?;
+    fs::write("hpp.jpg", &hpp)?;
+
+    let twitter = tab
+        .navigate_to("https://twitter.com/home?lang=ja")?
+        .wait_until_navigated()?
+        .capture_screenshot(ScreenshotFormat::JPEG(Some(75)), None, true)?;
+    fs::write("twitter.jpg", &twitter)?;
+
+    println!("Screenshots successfully created.");
+    Ok(())
 }
