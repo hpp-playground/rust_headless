28187337524fab88d7f2f787a43f68b29e7d4b6d
hppRC <hpp.ricecake@gmail.com>
2019-12-16 01:14:08 +0900

    なんでも出来ます

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::fs;
-
 use failure::Fallible;
+use std::{env, fs, thread, time};
 
 use headless_chrome::{protocol::page::ScreenshotFormat, Browser, LaunchOptionsBuilder};
 
 fn main() -> Fallible<()> {
+    let five_seconds = time::Duration::new(5, 0);
+
     let options = LaunchOptionsBuilder::default()
         .headless(false)
         .build()
@@ -12,25 +13,24 @@ fn main() -> Fallible<()> {
     let browser = Browser::new(options)?;
     let tab = browser.wait_for_initial_tab()?;
 
-    tab.navigate_to("https://hpprc.com")?
-        .wait_until_navigated()?
-        .wait_for_element("div.css-rm8i1b")?
-        .click()?;
-    tab.wait_until_navigated()?
-        .wait_for_element("div.css-1cngdp0")?
-        .click()?;
-    let hpp = tab.wait_until_navigated()?.capture_screenshot(
-        ScreenshotFormat::JPEG(Some(75)),
-        None,
-        true,
-    )?;
-    fs::write("./screenshots/hpp.jpg", &hpp)?;
+    let username = "osaremochi";
+    let password = &env::var("TWITTER_PASSWORD")?;
 
-    let twitter = tab
-        .navigate_to("https://twitter.com/home?lang=ja")?
+    tab.navigate_to("https://twitter.com/home?lang=ja")?
         .wait_until_navigated()?
-        .capture_screenshot(ScreenshotFormat::JPEG(Some(75)), None, true)?;
-    fs::write("./screenshots/twitter.jpg", &twitter)?;
+        .wait_for_element(".js-username-field.email-input.js-initial-focus")?
+        .click()?;
+    tab.type_str(username)?
+        .wait_for_element(".js-password-field")?
+        .click()?;
+    tab.type_str(password)?
+        .wait_for_element("button.submit.EdgeButton.EdgeButton--primary.EdgeButtom--medium")?
+        .click()?;
+
+    thread::sleep(five_seconds);
+
+    let timeline = tab.capture_screenshot(ScreenshotFormat::JPEG(Some(75)), None, true)?;
+    fs::write("./screenshots/timeline.jpg", &timeline)?;
 
     println!("Screenshots successfully created.");
     Ok(())
